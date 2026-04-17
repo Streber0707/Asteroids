42994061a3dc6a5d74bd0da4474a25b44bf9663a
Streber0707 <jessica.schmuhl@proton.me>
2026-04-17 21:45:41 +0200

    added player class and object drawn on game screen

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import pygame
 from logger import log_state
 from constants import *
+from player import *
 
 def main():
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player_1 = Player(x, y, PLAYER_RADIUS)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_1.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick() / 1000

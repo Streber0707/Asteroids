d12c9bdee05cbb5b9963eea85f1c8f26eba3b249
Streber0707 <jessica.schmuhl@proton.me>
2026-04-18 01:54:29 +0200

    added groups for easier method integration in main

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from player import *
 def main():
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
@@ -16,9 +19,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player_1.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player_1.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
